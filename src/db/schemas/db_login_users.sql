e7184ca175b761cbba286b5feebdf6925b273d19
Alvaruto <alvaruto@gmail.com>
2019-12-28 13:42:00 -0500

    first commit

---
+++ src/db/schemas/db_login_users.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE  IF NOT EXISTS `db_login` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `db_login`;
+-- MySQL dump 10.13  Distrib 5.7.28, for Linux (x86_64)
+--
+-- Host: localhost    Database: db_login
+-- ------------------------------------------------------
+-- Server version	5.7.28-0ubuntu0.18.04.4
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `idusers` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `idtoken` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idusers`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  KEY `fk_users_1_idx` (`idtoken`),
+  CONSTRAINT `fk_users_1` FOREIGN KEY (`idtoken`) REFERENCES `tokens` (`idtokens`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (7,'alvaruto@gmail.com','123456',7),(9,'alvaruto2@gmail.com','123456',9),(10,'alvaruto4@gmail.com','123456',10),(14,'alvaruto5@gmail.com','123456',14);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-12-27 23:34:09
---
+++ src/webapp/controllers/routes.py
@@ -0,0 +1,118 @@
+from flask import render_template, flash, request, session, make_response, g,copy_current_request_context,redirect
+
+
+from app.db.dbMySQL import MySQLUser,MySQLToken
+from app.models import User,Token
+from app.utils.generate_token import generateToken
+import threading
+
+from . import auth_bp
+import datetime
+
+from threading import Timer,Thread,Event
+
+
+@auth_bp.route('/sign_in', methods=('GET', 'POST'))
+def signIn():   
+    
+    m = MySQLUser()
+    mT = MySQLToken()
+    
+    # verficia si el toquen que se tiene en las cookies esta registrado en la bd y devulve el usuario corresponidente
+    user = mT.existToken(request.cookies.get('token'))
+    print(request.cookies.get('token'))
+
+    if user:
+
+        # cambio de token de usuario cada cierto tiempo
+        @copy_current_request_context
+        def verificate():
+            mT = MySQLToken()
+            tokenValue = generateToken()
+            t = Token(tokenValue,datetime.datetime.now(),user.token)
+            #actualiza el valro del token en la bd
+            mT.update(t)                       
+        # inicio de ciclo infinito cambio de token
+        g.user.setFunction(verificate) 
+        g.user.start()        
+
+        return render_template('home.html',user=user)
+
+    if request.method == 'POST':
+        
+        # generacion de token
+        tokenValue = generateToken()
+
+        t = Token(tokenValue,datetime.datetime.now(),0)
+        email = request.form['username']
+
+        # busqueda de usario por gmail (ingresado anteriormente)       
+        user = m.getOne(email)
+        
+        if user != None:
+            
+            # verificacion de password
+            if user.password == request.form['password']:
+                
+                t.id=user.token
+                mT.update(t)
+                session['user'] = user.id                
+                response = make_response(render_template('home.html',user=user))
+
+                # expiracion de cookie (no es posble ponerl una espiracion menor a 0.3 dias)
+                expireDate = t.date + datetime.timedelta(days=0.3)
+                response.set_cookie("token",tokenValue,expires = expireDate) 
+
+                # cambio de token de usario cada 
+                @copy_current_request_context
+                def verificate():
+                    mT = MySQLToken()
+                    tokenValue = generateToken()
+                    t = Token(tokenValue,datetime.datetime.now(),user.token)
+                    mT.update(t)                       
+                    
+                g.user.setFunction(verificate) 
+                g.user.start()                                             
+                
+                
+                return response        
+        flash('Datos ingresados incorrectos')
+        return render_template('sign_in.html')        
+
+    return render_template('sign_in.html')
+
+
+@auth_bp.route('/register', methods=('GET', 'POST'))
+def register():
+    
+    m = MySQLUser()
+    if request.method == 'POST':  
+        
+        # generacion de token
+        tokenValue = generateToken()
+        t = Token(tokenValue,datetime.datetime.now(),0)
+        # creacion de usario
+        user = User(request.form['username'],request.form['password'],t,'0')              
+        if m.create(user):
+            session['user'] = user.id
+
+            response = make_response(render_template('home.html',user=user))
+            # expiracion de cookie (no es posble ponerl una espiracion menor a 0.3 dias)
+            expireDate = t.date + datetime.timedelta(days=0.3)
+            response.set_cookie("token",tokenValue, expires=expireDate)
+            
+                    #return 'Email: {} \nPassword: {}'.format(user.email,user.password)
+            return response
+        flash('Email ya creado')
+        
+
+    return render_template('register.html')
+
+@auth_bp.route('/logout')
+def logout():
+    response = make_response(render_template('sign_in.html'))
+    response.set_cookie("token",'')
+    return response
+
+
+    
